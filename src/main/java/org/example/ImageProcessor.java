4ff6076a305867d22764f6aa91d5221f399c06d2
Jakub <siemagosciu32@gmail.com>
2023-07-20 05:14:12 +0200

    brightness save

--- src/main/java/org/example/ImageProcessor.java
+++ src/main/java/org/example/ImageProcessor.java
@@ -5,7 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class ImageProcessor {
+public class ImageProcessor{
     private BufferedImage image;
     private int height, width;
 
@@ -15,7 +15,44 @@ public class ImageProcessor {
         height = image.getHeight();
         width = image.getWidth();
     }
+    public static int Truncate(int value)
+    {
 
+        if (value < 0) value = 0;
+        else if (value > 255) value = 255;
+        return value;
+    }
+
+    // Method 2
+    // To adjust the brightness of image
+    public void AdjustBrightness(int brightnessValue)
+    {
+        // Declaring an array for spectrum of colors
+        int rgb[];
+
+        // Outer loop for width of image
+        for (int i = 0; i < image.getWidth(); i++) {
+            run();
+            // Inner loop for height of image
+            for (int j = 0; j < image.getHeight(); j++) {
+
+                rgb = image.getRaster().getPixel(i, j, new int[3]);
+
+                // Using(calling) method 1
+                int red
+                        = Truncate(rgb[0] + brightnessValue);
+                int green
+                        = Truncate(rgb[1] + brightnessValue);
+                int blue
+                        = Truncate(rgb[2] + brightnessValue);
+
+                int arr[] = { red, green, blue };
+
+                // Using setPixel() method
+                image.getRaster().setPixel(i, j, arr);
+            }
+        }
+    }
     public void saveImage(String path) throws IOException {
         ImageIO.write(image, "png", new File(path));
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws IOException {
+        ImageProcessor imageProcessor = new ImageProcessor();
+        imageProcessor.load("C:\\Users\\Jakub\\Desktop\\ImageProcessing\\src\\Photo on 12-06-2023 at 13.32.jpg");
+        imageProcessor.AdjustBrightness(100);
+        imageProcessor.saveImage("C:\\Users\\Jakub\\Desktop\\ImageProcessing\\output.png");
+
     }
 }
