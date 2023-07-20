af56a6f197b4c8a1d0f57dc5f142b9d578e54b7d
Jakub <siemagosciu32@gmail.com>
2023-07-20 15:32:58 +0200

    adjust Brightness using threads

--- src/main/java/org/example/ImageProcessor.java
+++ src/main/java/org/example/ImageProcessor.java
@@ -15,9 +15,8 @@ public class ImageProcessor{
         height = image.getHeight();
         width = image.getWidth();
     }
-    public static int Truncate(int value)
+    public static int truncate(int value)
     {
-
         if (value < 0) value = 0;
         else if (value > 255) value = 255;
         return value;
@@ -25,14 +24,14 @@ public class ImageProcessor{
 
     // Method 2
     // To adjust the brightness of image
-    public void AdjustBrightness(int brightnessValue)
+    public void adjustBrightness(int brightnessValue)
     {
         // Declaring an array for spectrum of colors
         int rgb[];
 
         // Outer loop for width of image
         for (int i = 0; i < image.getWidth(); i++) {
-            run();
+
             // Inner loop for height of image
             for (int j = 0; j < image.getHeight(); j++) {
 
@@ -40,17 +39,56 @@ public class ImageProcessor{
 
                 // Using(calling) method 1
                 int red
-                        = Truncate(rgb[0] + brightnessValue);
+                        = truncate(rgb[0] + brightnessValue);
                 int green
-                        = Truncate(rgb[1] + brightnessValue);
+                        = truncate(rgb[1] + brightnessValue);
                 int blue
-                        = Truncate(rgb[2] + brightnessValue);
+                        = truncate(rgb[2] + brightnessValue);
 
                 int arr[] = { red, green, blue };
 
                 // Using setPixel() method
                 image.getRaster().setPixel(i, j, arr);
             }
+        }
+    }
+    public void addBrightnessWithThreads(int brightnessValue) throws InterruptedException {
+        int cores = Runtime.getRuntime().availableProcessors();
+        int chunk = height / cores;
+
+        Thread threads[] = new Thread[cores];
+
+        for (int i = 0; i < cores; ++i) {
+            int threadIndex = i;
+            threads[i] = new Thread(() -> {
+                int startline = threadIndex * chunk;
+                int endline = (threadIndex == cores - 1) ? height : startline + chunk;
+
+                for (int y = startline; y < endline; y++) {
+                    for (int x = 0; x < width; x++) {
+                        int rgb[] = image.getRaster().getPixel(x, y, new int[3]);
+
+                        // Using(calling) method 1
+                        int red
+                                = truncate(rgb[0] + brightnessValue);
+                        int green
+                                = truncate(rgb[1] + brightnessValue);
+                        int blue
+                                = truncate(rgb[2] + brightnessValue);
+
+                        int arr[] = { red, green, blue };
+
+                        // Using setPixel() method
+                        image.getRaster().setPixel(x, y, arr);
+                    }
+                }
+            });
+        }
+        for (int j = 0; j < cores; ++j) {
+            threads[j].start();
+        }
+        for (var thread : threads) {
+            thread.join();
         }
     }
     public void saveImage(String path) throws IOException {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,10 @@ package org.example;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         ImageProcessor imageProcessor = new ImageProcessor();
         imageProcessor.load("C:\\Users\\Jakub\\Desktop\\ImageProcessing\\src\\Photo on 12-06-2023 at 13.32.jpg");
-        imageProcessor.AdjustBrightness(100);
+        imageProcessor.addBrightnessWithThreads(-100);
         imageProcessor.saveImage("C:\\Users\\Jakub\\Desktop\\ImageProcessing\\output.png");
 
     }
