941761c48ba48a5d8319c8b0d6076752a89f5399
kuba <jakubobara1@wp.pl>
2023-07-27 18:31:37 +0200

    working histogram equalization

--- src/main/java/org/example/ImageProcessor.java
+++ src/main/java/org/example/ImageProcessor.java
@@ -1,29 +1,31 @@
 package org.example;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ImageProcessor{
     private BufferedImage image;
     private int height, width;
+
+    public ImageProcessor(BufferedImage image) {
+        this.image = image;
+    }
+    public ImageProcessor() {}
 
     public void load(String path) throws IOException {
         File file = new File(path);
         image = ImageIO.read(file);
         height = image.getHeight();
         width = image.getWidth();
-    }
-    public static int truncate(int value)
-    {
-        if (value < 0) value = 0;
-        else if (value > 255) value = 255;
-        return value;
-    }
 
-
+    }
     public void adjustBrightness(int brightnessValue)
     {
 
@@ -31,19 +33,18 @@ public class ImageProcessor{
 
         // Outer loop for width of image
         for (int i = 0; i < image.getWidth(); i++) {
-
             // Inner loop for height of image
             for (int j = 0; j < image.getHeight(); j++) {
-
                 rgb = image.getRaster().getPixel(i, j, new int[3]);
 
                 // Using(calling) method 1
                 int red
-                        = Math.min(0, rgb[0] + brightnessValue);
+                        = Math.max(0,Math.min(255,(rgb[0] + brightnessValue)));
                 int green
-                        = Math.min(0, rgb[1] + brightnessValue);
+                        = Math.max(0,Math.min(255,(rgb[1] + brightnessValue)));
                 int blue
-                        = Math.min(0, rgb[2] + brightnessValue);
+                        = Math.max(0,Math.min(255,(rgb[2] + brightnessValue)));
+
 
                 int arr[] = { red, green, blue };
 
@@ -71,11 +72,11 @@ public class ImageProcessor{
 
                         // Using(calling) method 1
                         int red
-                                = truncate(rgb[0] + brightnessValue);
+                                = Math.max(0,Math.min(255,(rgb[0] + brightnessValue)));
                         int green
-                                = truncate(rgb[1] + brightnessValue);
+                                = Math.max(0,Math.min(255,(rgb[1] + brightnessValue)));
                         int blue
-                                = truncate(rgb[2] + brightnessValue);
+                                = Math.max(0,Math.min(255,(rgb[2] + brightnessValue)));
 
                         int arr[] = { red, green, blue };
 
@@ -96,38 +97,139 @@ public class ImageProcessor{
     public void calculateHistogram() throws InterruptedException {
         int cores = Runtime.getRuntime().availableProcessors();
         int chunk = height / cores;
-        int[][] histogram = new int[cores][256];
+
+        int[][] rhistogram = new int[cores][256];
+        int[][] ghistogram = new int[cores][256];
+        int[][] bhistogram = new int[cores][256];
+
 
         Thread threads[] = new Thread[cores];
         for (int i = 0; i < cores; ++i) {
             int threadIndex = i;
             threads[i] = new Thread(() -> {
-                Arrays.fill(histogram[threadIndex], 0);
+                Arrays.fill(rhistogram[threadIndex], 0);
+                Arrays.fill(ghistogram[threadIndex], 0);
+                Arrays.fill(bhistogram[threadIndex], 0);
+
                 int startline = threadIndex * chunk;
                 int endline = (threadIndex == cores - 1) ? height : startline + chunk;
 
                 for (int y = startline; y < endline; y++) {
                     for (int x = 0; x < width; x++) {
                         int rgb = image.getRGB(x, y);
-                        int r = (0xFF0000 & rgb) >> 16;
-                        ++histogram[threadIndex][r];
+
+                        int r = (rgb >> 16) & 0xFF;
+                        ++rhistogram[threadIndex][r];
+
+                        int g = (rgb >> 8) & 0xFF;
+                        ++ghistogram[threadIndex][g];
+
+                        int b = rgb & 0xFF;
+                        ++bhistogram[threadIndex][b];
                     }
                 }
             });
         }
         for (int i = 0; i < cores; ++i)
             threads[i].start();
-
         for (int i = 0; i < cores; ++i)
             threads[i].join();
-        int[] finalHistogram = new int[256];
-        for (int i = 0; i < cores; ++i)
-            for (int j = 0; j < 256; ++j)
-                finalHistogram[j] += histogram[i][j];
 
-        for (int i = 0; i < 256; ++i)
-            System.out.println(i + ", " + finalHistogram[i] + " ");
+        int[] finalRedHistogram = new int[256];
+        int[] finalGreenHistogram = new int[256];
+        int[] finalBlueHistogram = new int[256];
+
+        for (int i = 0; i < cores; ++i)
+            for (int j = 0; j < 256; ++j){
+                finalRedHistogram[j] += rhistogram[i][j];
+                finalGreenHistogram[j] += ghistogram[i][j];
+                finalBlueHistogram[j] += bhistogram[i][j];
+            }
+
+        //count distributors
+        int[] rLut = getLut(finalRedHistogram);
+        int[] gLut = getLut(finalGreenHistogram);
+        int[] bLut = getLut(finalBlueHistogram);
+
+
+        for(int i=0; i<image.getWidth(); i++) {
+            for(int j=0; j<image.getHeight(); j++) {
+
+                // Get pixels by R, G, B
+                int alpha = new Color(image.getRGB (i, j)).getAlpha();
+                int red = new Color(image.getRGB (i, j)).getRed();
+                int green = new Color(image.getRGB (i, j)).getGreen();
+                int blue = new Color(image.getRGB (i, j)).getBlue();
+
+                // Set new pixel values using the histogram lookup table
+                red = rLut[red];
+                green = gLut[green];
+                blue = bLut[blue];
+
+                // Return back to original format
+                int newPixel = colorToRGB(alpha, red, green, blue);
+
+                // Write pixels into image
+                image.setRGB(i, j, newPixel);
+
+            }
+        }
+
+        for (int i = 0; i < 256; ++i){
+            System.out.println(i + ", Red: " + rLut[i] + ", Green: " + gLut[i] + ", Blue: " + bLut[i] + "." );
+        }
+
 }
+    private static int colorToRGB(int alpha, int red, int green, int blue) {
+
+        int newPixel = 0;
+        newPixel += alpha; newPixel = newPixel << 8;
+        newPixel += red; newPixel = newPixel << 8;
+        newPixel += green; newPixel = newPixel << 8;
+        newPixel += blue;
+
+        return newPixel;
+
+    }
+    private int[] getLut(int[] channel){
+
+        int totalPix = height * width;
+        double[] distributor = new double[256];
+        Arrays.fill(distributor, 0);
+        distributor[0] = channel[0];
+        for (int i = 1; i < 256; ++i){
+            distributor[i] = distributor[i-1] + channel[i];
+        }
+
+        int temp = 0;
+        while (distributor[temp] == 0){
+            ++temp;
+        }
+        double firstNotNull = distributor[temp];
+
+        //LUT table
+        int[] lut = new int[256];
+        for (int i = 0; i < 256 ; ++i){
+            double v = ((distributor[i] - firstNotNull) / (totalPix - firstNotNull)) * 255;
+            lut[i] = (int) Math.round(v);
+        }
+        return lut;
+    }
+
+    public static BufferedImage getGrayScale(BufferedImage image){
+        BufferedImage gImg = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+        WritableRaster wr = image.getRaster();
+        WritableRaster gr = gImg.getRaster();
+
+        for(int i=0;i<wr.getWidth();i++){
+            for(int j=0;j<wr.getHeight();j++){
+                gr.setSample(i, j, 0, wr.getSample(i, j, 0));
+            }
+        }
+        gImg.setData(gr);
+        return gImg;
+    }
+
 
     public void saveImage(String path) throws IOException {
         ImageIO.write(image, "png", new File(path));
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -8,20 +9,23 @@ public class Main {
         ImageProcessor imageProcessor = new ImageProcessor();
 
         long start = System.currentTimeMillis();
-        imageProcessor.load("C:\\Users\\Jakub\\Desktop\\ImageProcessing\\src\\Photo on 12-06-2023 at 13.32.jpg");
-        imageProcessor.adjustBrightness(100);
-        long finish = System.currentTimeMillis();
-        long time1 = finish - start;
-
-        start = System.currentTimeMillis();
-        imageProcessor.addBrightnessWithThreads(100);
-        imageProcessor.saveImage("C:\\Users\\Jakub\\Desktop\\ImageProcessing\\output.png");
-        finish = System.currentTimeMillis();
-        long time2 = finish - start;
-
-        System.out.println(String.format("withoutThreads = %d,  with threads = %d", time1, time2));
+        imageProcessor.load("/Users/kuba/Downloads/Unequalized_Hawkes_Bay_NZ.jpg");
+        //imageProcessor.addBrightnessWithThreads(10);
+//        long finish = System.currentTimeMillis();
+//        long time1 = finish - start;
+//
+//        start = System.currentTimeMillis();
+//        imageProcessor.addBrightnessWithThreads(100);
+        //imageProcessor.saveImage("/Users/kuba/IdeaProjects/ImageProcessing/src/main/java/output.png");
+//        finish = System.currentTimeMillis();
+//        long time2 = finish - start;
+//
+//        System.out.println(String.format("withoutThreads = %d,  with threads = %d", time1, time2));
 
         imageProcessor.calculateHistogram();
+        imageProcessor.saveImage("/Users/kuba/IdeaProjects/ImageProcessing/src/main/java/output.png");
+
+
 
     }
 }
